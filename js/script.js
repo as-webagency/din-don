7b872d8ba8d1a3ac3bb775d5388810d6c8b34a3c
as-webagency <as.webagency2020@gmail.com>
2020-10-27 17:30:20 +0200

    adв popups

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// const overlay = document.querySelector( '.overlay' );
+// const popupThanks = document.querySelector( '.popup-thanks' );
+// const contactsHeadButton = document.querySelector( '.contacts-head__button' );
+// const body = document.querySelector( 'body' );
+
+// contactsHeadButton.addEventListener( 'click', ( event ) => {
+//     event.preventDefault();
+//     body.classList.add( 'scroll' );
+
+//     overlay.style.display = 'block';
+//     popupThanks.style.display = 'block';
+//     console.log(event);
+
+//     if ( overlay ) {
+//         overlay.closest(false);
+//     }
+// });
